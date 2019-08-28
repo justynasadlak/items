1b8b4978a6d286d19daf80993d13fb1d624d9a53
Unknown <justynasadlak10@gmail.com>
2019-08-28 19:47:04 +0200

    Add login form and login method

--- src/app/modules/login/components/login-view/login-view.component.ts
+++ src/app/modules/login/components/login-view/login-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-view',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-view.component.scss']
 })
 export class LoginViewComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  @Output()
+  login: EventEmitter<string> = new EventEmitter<string>();
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
   }
 
+  onLogin(): void {
+    this.login.emit(this.loginForm.value);
+  }
+
+  private initForm(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    });
+  }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './containers/login/login.component';
 import { LoginViewComponent } from './components/login-view/login-view.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, LoginViewComponent],
-  imports: [CommonModule, SharedModule, LoginRoutingModule],
+  imports: [CommonModule, SharedModule, LoginRoutingModule, ReactiveFormsModule],
   exports: [LoginComponent]
 })
 export class LoginModule {}
