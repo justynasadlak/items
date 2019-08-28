befee6e47376a24f45e91d2094d88468375e3f03
Unknown <justynasadlak10@gmail.com>
2019-08-28 19:01:54 +0200

    Create login routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/login/login.module').then(mod => mod.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './containers/login/login.component';
 import { LoginViewComponent } from './components/login-view/login-view.component';
-
-
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, LoginViewComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, LoginRoutingModule],
+  exports: [LoginComponent]
 })
-export class LoginModule { }
+export class LoginModule {}
