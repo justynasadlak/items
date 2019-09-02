13569c1ee84f199fb1a219a170e3339afadf4abf
Unknown <justynasadlak10@gmail.com>
2019-09-02 23:33:36 +0200

    Remove unused code

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -13,7 +13,6 @@ export class AuthService implements CanActivate {
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.userDataService.isAuthenticated().pipe(
       switchMap((res: boolean) => {
-        console.log(res);
         if (!res) {
           this.router.navigate(['login']);
         }
