5154a7b7db7fd8c5f937239b7f6a04b6e7f4a67f
Unknown <justynasadlak10@gmail.com>
2019-09-02 12:36:55 +0200

    Create interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
 import { LoginModule } from './modules/login/login.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpRequestInterceptor } from './modules/core/interceptor/http-request.interceptor';
 import { ItemModule } from './modules/item/item.module';
 
 @NgModule({
@@ -14,9 +16,17 @@ import { ItemModule } from './modules/item/item.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule,
     LoginModule,
     ItemModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modules/core/interceptor/http-request.interceptor.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpRequestInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const headers = {
+      Authorization: `${localStorage.getItem('token')}`
+    };
+    return next.handle(localStorage.getItem('token') ? req.clone({ setHeaders: headers }) : req);
+  }
+}
