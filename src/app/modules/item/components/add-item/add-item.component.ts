dfb07ae6535dc3a6d1d9f82c3825ff68ee8102af
Unknown <justynasadlak10@gmail.com>
2019-09-02 12:51:56 +0200

    Create add item form

--- src/app/modules/item/components/add-item/add-item.component.ts
+++ src/app/modules/item/components/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-item.component.scss']
 })
 export class AddItemComponent implements OnInit {
-  constructor() {}
+  addItemForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.addItemForm = this.formBuilder.group({
+      itemName: ['']
+    });
+  }
 }
--- src/app/modules/item/item.module.ts
+++ src/app/modules/item/item.module.ts
@@ -4,10 +4,12 @@ import { ItemComponent } from './containers/item/item.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
 import { ItemRoutingModule } from './item-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ItemComponent, ItemListComponent, AddItemComponent],
-  imports: [CommonModule, ItemRoutingModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule, ItemRoutingModule],
   exports: [ItemComponent]
 })
 export class ItemModule {}
