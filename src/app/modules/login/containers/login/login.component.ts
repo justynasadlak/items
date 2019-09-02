b0423dc348d99686388c18c865e0651d082ac256
Unknown <justynasadlak10@gmail.com>
2019-09-02 19:47:33 +0200

    Add router after login

--- src/app/modules/login/containers/login/login.component.ts
+++ src/app/modules/login/containers/login/login.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserDataService } from '../../../../services/user-data.service';
 import { LoginData } from '../../../../models/login-data';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  constructor(private userDataService: UserDataService) {}
-
-  ngOnInit() {}
+export class LoginComponent {
+  constructor(private userDataService: UserDataService, private router: Router) {}
 
   onLogin(loginData: LoginData): void {
     console.log(loginData);
     this.userDataService.loginUser(loginData).subscribe(
       res => {
+        this.router.navigate(['items']);
         localStorage.setItem('token', res);
       },
       error => console.log(error)
